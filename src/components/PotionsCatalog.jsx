957272d2d3c28c1c918bcb1b89bad587be6c9600
Abhishek garg <codefeast311@gmail.com>
2024-01-22 22:53:20 +0530

    Adding loader

--- src/components/PotionsCatalog.jsx
+++ src/components/PotionsCatalog.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Pagination from "@mui/material/Pagination";
 import PotionModal from "./PotionModal";
+import CircularProgress from "@mui/material/CircularProgress";
 const PotionsCatalog = () => {
   const [allPotions, setAllPotions] = useState([]);
   const [potions, setPotions] = useState([]);
@@ -10,6 +11,7 @@ const PotionsCatalog = () => {
   const [characteristicFilter, setCharacteristicFilter] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPotion, setSelectedPotion] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,6 +24,7 @@ const PotionsCatalog = () => {
       } catch (error) {
         console.log(error);
       } finally {
+        setLoading(false);
       }
     };
 
@@ -33,34 +36,34 @@ const PotionsCatalog = () => {
       (potion) =>
         potion?.attributes?.name
           .toLowerCase()
-          .includes(searchTerm.toLowerCase()) 
-        //   ||
-        // potion?.attributes?.characteristics
-        //   ?.toLowerCase()
-        //   .includes(searchTerm.toLowerCase())
+          .includes(searchTerm.toLowerCase())
+      //   ||
+      // potion?.attributes?.characteristics
+      //   ?.toLowerCase()
+      //   .includes(searchTerm.toLowerCase())
       // potion?.attributes?.difficulty.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     if (difficultyFilter) {
-        const filteredPotionsByDifficulty = filteredPotions.filter(
-            (potion) => potion?.attributes?.difficulty === difficultyFilter
-        );
-        setPotions(filteredPotionsByDifficulty);
-        return;
+      const filteredPotionsByDifficulty = filteredPotions.filter(
+        (potion) => potion?.attributes?.difficulty === difficultyFilter
+      );
+      setPotions(filteredPotionsByDifficulty);
+      return;
     }
 
     if (characteristicFilter) {
-        const filteredPotionsByCharacteristic = filteredPotions.filter(
-            (potion) => potion?.attributes?.characteristics === characteristicFilter
-        );
-        setPotions(filteredPotionsByCharacteristic);
-        return;
+      const filteredPotionsByCharacteristic = filteredPotions.filter(
+        (potion) => potion?.attributes?.characteristics === characteristicFilter
+      );
+      setPotions(filteredPotionsByCharacteristic);
+      return;
     }
 
     setPotions(filteredPotions);
   }, [searchTerm, difficultyFilter, allPotions, characteristicFilter]);
 
-  console.log(potions)
+  console.log(potions);
   return (
     <div className="p-4 flex flex-col gap-4">
       <h1 className="text-4xl font-bold">Harry Potter Potions Catalog</h1>
@@ -73,8 +76,9 @@ const PotionsCatalog = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
         <select
-        onChange={(e) => setDifficultyFilter(e.target.value)}
-        className="border-2 border-gray-300 rounded-md p-2">
+          onChange={(e) => setDifficultyFilter(e.target.value)}
+          className="border-2 border-gray-300 rounded-md p-2"
+        >
           <option value="">Filter by Difficulty</option>
           <option value="Beginner">Beginner</option>
           <option value="Intermediate">Intermediate</option>
@@ -82,17 +86,21 @@ const PotionsCatalog = () => {
         </select>
 
         <select
-        onChange={(e) => setCharacteristicFilter(e.target.value)}
-        className="border-2 border-gray-300 rounded-md p-2">
-        <option value="">Filter by Characteristic</option>
-        <option value="Green">Green</option>
-        <option value="Blue">Blue</option>
-      </select>
+          onChange={(e) => setCharacteristicFilter(e.target.value)}
+          className="border-2 border-gray-300 rounded-md p-2"
+        >
+          <option value="">Filter by Characteristic</option>
+          <option value="Green">Green</option>
+          <option value="Blue">Blue</option>
+        </select>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {potions.map((item, index) => (
-          <div key={index} className="border-2 border-gray-300 rounded-md p-4 flex flex-col">
+          <div
+            key={index}
+            className="border-2 border-gray-300 rounded-md p-4 flex flex-col"
+          >
             <img
               src={item.attributes.image}
               alt={item.attributes.name}
@@ -105,25 +113,31 @@ const PotionsCatalog = () => {
               Characteristic: {item.attributes.characteristics}
             </p>
             <div className="mt-auto self-end">
-            <PotionModal item={item}/>
+              <PotionModal item={item} />
             </div>
           </div>
         ))}
-
-        {potions.length === 0 && (
-          <div className="text-center col-span-full">
-            <h1 className="text-2xl font-bold">No potions found</h1>
-          </div>
-        )}
       </div>
+
+      {loading && (
+        <div className="flex justify-center">
+          <CircularProgress />
+        </div>
+      )}
+
+      {potions.length === 0 && !loading && (
+        <div className="text-center col-span-full">
+          <h1 className="text-2xl font-bold">No potions found</h1>
+        </div>
+      )}
       <div className="flex justify-center">
-      <Pagination
-        count={15}
-        variant="outlined"
-        onChange={(event, page) => {
+        <Pagination
+          count={15}
+          variant="outlined"
+          onChange={(event, page) => {
             setCurrentPage(page);
-        }}
-      />
+          }}
+        />
       </div>
     </div>
   );
